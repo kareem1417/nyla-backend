13ab4416b41c69d62a032cc0925d95c87d0950a1
Kareem <kareem@example.com>
2026-05-13 18:13:57 +0300

    update reset password email

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -2,9 +2,9 @@ import nodemailer from 'nodemailer';
 
 const sendEmail = async (options) => {
     const transporter = nodemailer.createTransport({
-        host: process.env.EMAIL_HOST,
-        port: process.env.EMAIL_PORT, // هيقرا 2525 من المتغيرات
-        secure: false, // لازم false عشان ده بورت 2525
+        host: 'smtp-relay.brevo.com', // 👈 ثبتناها بإيدنا هنا
+        port: 2525,                   // 👈 وثبتنا البورت 2525
+        secure: false,
         auth: {
             user: process.env.EMAIL_USER,
             pass: process.env.EMAIL_PASS,
@@ -12,7 +12,6 @@ const sendEmail = async (options) => {
     });
 
     const mailOptions = {
-        // ده الاسم والإيميل اللي هيظهر للعميل (إيميل البراند بتاعكم)
         from: `"NYLA Beauty" <nylaabeauty@gmail.com>`,
         to: options.email,
         subject: options.subject,
